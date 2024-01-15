d50745849a99f4e063f83270c7f76beeff62bebe
ananyatewari <ananyatewari0205@gmail.com>
2024-01-15 15:30:56 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,47 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor(){
+    super();
+    this.state = {
+        clickRate : 0
+    }
+  }
+
+  render(){
+    return (
+        <>
+        <div id="home">
+            <div id="left" onClick={() => { 
+                if (this.state.clickRate > 0)
+                    this.setState({clickRate : this.state.clickRate - 1}) 
+                else if (this.state.clickRate <= 0) 
+                this.setState({clickRate : 2})
+        }}>
+                <ArrowBackIosIcon/>
+            </div>
+
+            <div id="carouselImage" style={{backgroundImage: `url('${images[this.state.clickRate].img}')`, backgroundRepeat: 'no-repeat'}}>
+                <div id="content">
+                    <h1>{images[this.state.clickRate].title}</h1>
+                    <div>{images[this.state.clickRate].subtitle}</div>
+                </div>
+            </div>
+
+            <div id="right" onClick={() => { 
+                if (this.state.clickRate < images.length - 1)
+                this.setState({clickRate : this.state.clickRate + 1}) 
+            else if (this.state.clickRate >= images.length - 1) 
+            this.setState({clickRate : 0})
+    }}>
+                <ArrowForwardIosIcon/>
+            </div>
+        </div>
+        <span>Made by Ananya :) </span>
+        </>
+    )
+  }
+
 }
 
 export default Carousel;
